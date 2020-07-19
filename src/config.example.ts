24e9870e1af5abe3b7ed25007f1d09a77a30b67e
QuantumlyTangled <ethoarx@gmail.com>
2020-07-19 21:07:07 +0200

    Initial example

---
+++ src/config.example.ts
@@ -0,0 +1,6 @@
+export const INFLUX = {
+	BASE_URL: 'http://localhost:8285',
+
+	INFLUX_LOGIN_USERNAME: '',
+	INFLUX_LOGIN_PASSWORD: ''
+};
---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import 'module-alias';
+
+import * as puppeteer from 'puppeteer';
+
+import { INFLUX } from '@root/config';
+
+void (async () => {
+	const browser = await puppeteer.launch({
+		timeout: 0,
+		dumpio: true,
+		defaultViewport: {
+			width: 1920,
+			height: 1080
+		}
+	});
+	const page = await browser.newPage();
+	await page.goto(INFLUX.BASE_URL, { waitUntil: 'networkidle2' });
+
+	await page.type('[name=username]', INFLUX.INFLUX_LOGIN_USERNAME);
+	await page.type('[name=password]', INFLUX.INFLUX_LOGIN_PASSWORD);
+	await page.click('.cf-button-primary');
+
+	await page.waitForNavigation({
+		waitUntil: [
+			'load',
+			'domcontentloaded',
+			'networkidle2'
+		]
+	});
+
+	await page.goto(`${INFLUX.BASE_URL}/orgs/060048754532a000/dashboards/06009f370a5b0000`, { waitUntil: 'networkidle2' });
+	await page.click('[data-testid=presentation-mode-toggle]');
+
+	await page.waitFor(10000);
+
+	await page.screenshot({ path: 'heh.png' });
+
+	await browser.close();
+})();
